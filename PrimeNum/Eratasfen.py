d2e0b252d81a83dddd8884cc90d551c06c4acbfd
bekzod818 <bekzod030900@gmail.com>
2022-10-18 07:19:57 +0500

    updated

--- PrimeNum/Eratasfen.py
+++ PrimeNum/Eratasfen.py
@@ -1,5 +1,13 @@
 from math import isqrt
 
+def printnum(func):
+    def wrapper(nums):
+        prime = func(nums)
+        return " ".join(str(i) for i in prime)
+    return wrapper
+
+
+@printnum
 def primes_less_than(n):
     if n <= 2:
         return []
@@ -7,13 +15,13 @@ def primes_less_than(n):
     is_prime[0] = False
     is_prime[1] = False
 
-    for i in range(2, isqrt(n)):
+    for i in range(2, isqrt(n) + 1):
         if is_prime[i]:
             for x in range(i*i, n, i):
                 is_prime[x] = False
-    
+
     return [i for i in range(n) if is_prime[i]]
 
 
-n = int(input("n = "))
+n = int(input())
 print(primes_less_than(n))
