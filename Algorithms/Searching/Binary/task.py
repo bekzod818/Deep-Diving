c6df6c536b406eacc5a6f6b6f2e874f7e40f7ecc
bekzod818 <bekzod030900@gmail.com>
2023-12-16 15:21:02 +0500

    fix: update_binary_search

--- Algorithms/Searching/Binary/task.py
+++ Algorithms/Searching/Binary/task.py
@@ -1,16 +1,20 @@
 def binarySearch(arr: list, item: int) -> int:
-    L = 0
-    R = len(arr) - 1
-    while L <= R:
-        m = (L + R) // 2
-        if arr[m] == item:
-            return m
-        elif arr[m] < item:
-            L = m + 1
-        elif arr[m] > item:
-            R = m - 1
-    return None
+    l_index = 0
+    r_index = len(arr) - 1
+
+    while l_index <= r_index:
+        middle_index = (l_index + r_index) // 2
+
+        if arr[middle_index] == item:
+            return middle_index
+        elif arr[middle_index] < item:
+            l_index = middle_index + 1
+        elif arr[middle_index] > item:
+            r_index = middle_index - 1
+
+    return -1
 
 
-result = binarySearch([1, 3, 4, 6, 7, 8, 10, 12, 23, 45, 56, 78, 99], 23)
+# Binary search works with only sorted elements
+result = binarySearch([1, 3, 4, 6, 7, 8, 10, 12, 23, 45, 56, 78, 99], 25)
 print(result)
