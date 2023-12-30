261cc04e4adb9b0b0ad88cb1ec4cfe2d55202f81
bekzod818 <bekzod030900@gmail.com>
2023-12-30 22:30:57 +0500

    fix: db_list

--- Databases/NoSQL/MongoDB/demo_mongodb_test.py
+++ Databases/NoSQL/MongoDB/demo_mongodb_test.py
@@ -2,8 +2,16 @@ import pymongo
 
 myclient = pymongo.MongoClient("mongodb://localhost:2717/")
 
-mydb = myclient["test"]
+# Remember: In MongoDB, a database is not created until it gets content, so if this is your first time creating a database, you should complete the next two chapters (create collection and create document) before you check if the database exists!
 
-# database created!
+mydb = myclient["test"]  # database created!
+
+# You can check if a database exist by listing all databases in you system:
 
 print(myclient.list_database_names())
+
+# Check if "test" exists:
+
+dblist = myclient.list_database_names()
+if "test" in dblist:
+    print("The database exists.")
