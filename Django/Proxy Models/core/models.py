bb47a38fb46636c073bb218aa577953f3fc7659a
bekzod818 <bekzod030900@gmail.com>
2024-03-07 12:00:00 +0500

    fix: proxy models

--- Django/Proxy Models/core/models.py
+++ Django/Proxy Models/core/models.py
@@ -14,3 +14,53 @@ class Task(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class InProgressTask(Task):
+    class Meta:
+        proxy = True
+
+    class Manager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status=TaskStatus.IN_PROGRESS)
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            self.status = TaskStatus.IN_PROGRESS
+        super().save(*args, **kwargs)
+
+    objects = Manager()
+
+
+class ToDoTask(Task):
+    class Meta:
+        proxy = True
+        ordering = ("-created_at",)
+
+    class Manager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status=TaskStatus.TODO)
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            self.status = TaskStatus.TODO
+        super().save(*args, **kwargs)
+
+    objects = Manager()
+
+
+class CompletedTask(Task):
+    class Meta:
+        proxy = True
+        ordering = ("-created_at",)
+
+    class Manager(models.Manager):
+        def get_queryset(self):
+            return super().get_queryset().filter(status=TaskStatus.COMPLETED)
+
+    def save(self, *args, **kwargs):
+        if self._state.adding:
+            self.status = TaskStatus.COMPLETED
+        super().save(*args, **kwargs)
+
+    objects = Manager()
