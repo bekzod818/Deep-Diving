aab4eafe117205b2a2cec33a6a2d91ce411df1ba
bekzod818 <bekzod030900@gmail.com>
2024-04-27 22:53:12 +0500

    fix: urls and models updated

--- Django/DRF File Upload/config/settings.py
+++ Django/DRF File Upload/config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "core.apps.CoreConfig",
 ]
 
+AUTH_USER_MODEL = "core.User"
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -117,6 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- Django/DRF File Upload/core/models.py
+++ Django/DRF File Upload/core/models.py
@@ -1,1 +1,7 @@
-# Create your models here.
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    cv = models.FileField(upload_to="cvs/%Y/%m/%d/", null=True, blank=True)
+    uploaded_at = models.DateTimeField(null=True, blank=True)
