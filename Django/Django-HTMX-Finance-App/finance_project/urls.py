6d717e2c2f7d9466d1d77cfd08b1b2ea2303a797
bekzod818 <b.rakhimov>
2024-04-28 10:33:49 +0500

    delete project

--- Django/Django-HTMX-Finance-App/finance_project/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path('', include('tracker.urls')),
-    path('__debug__/', include('debug_toolbar.urls')),
-    path('accounts/', include('allauth.urls')),
-]
--- Django/Django-HTMX-Finance-App/tracker/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from tracker import views
-
-
-urlpatterns = [
-    path("", views.index, name='index'),
-    path("transactions/", views.transactions_list, name='transactions-list')
-]
