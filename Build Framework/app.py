9355382b9f4b87ffd0a0284bf4ece25c1ab8eed5
bekzod818 <bekzod030900@gmail.com>
2024-05-13 05:48:10 +0500

    fix: install webob

--- Build Framework/app.py
+++ Build Framework/app.py
@@ -1,6 +1,19 @@
+from webob import Request, Response
+
+
 class DolphinApp:
     def __call__(self, environ, start_response):
-        status = "200 OK"
-        headers = [("Content-Type", "text/plain")]
-        start_response(status, headers)
-        return [b"Hello Dolphin!"]
+        request = Request(environ)
+
+        response = Response()
+        response.text = "Hello Dolphin!"
+
+        return response(environ, start_response)
+
+
+# class DolphinApp:
+#     def __call__(self, environ, start_response):
+#         status = "200 OK"
+#         headers = [("Content-Type", "text/plain")]
+#         start_response(status, headers)
+#         return [b"Hello Dolphin!"]
